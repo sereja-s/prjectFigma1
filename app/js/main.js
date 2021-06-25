5275c7bee5e88117e505c031e0c169229b56ac08
sereja-s <serejasuvorov1979@gmail.com>
2021-06-25 20:02:02 +0300

    Первый слайдер (экран)

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
-$(function(){
+$(function () {
 
+	// вставим фоновую картинку
+	$('.header__slider').slick({
+		infinite: true,
+		fade: true,
+		prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+		nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
+		asNavFor: '.slider-dots' //связь
+	});
 
+	$('.slider-dots').slick({
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		asNavFor: '.header__slider' //связь
+	});
 
-  
 });
