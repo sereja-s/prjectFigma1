9c6e7a7b5b33a2c60589c7aa655f10a5c9628f15
sereja-s <serejasuvorov1979@gmail.com>
2021-06-26 23:55:25 +0300

    Второй экран (2-я часть)

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function () {
 
-	// вставим фоновую картинку
+	// работа стрелочек (лево-право)
 	$('.header__slider').slick({
 		infinite: true,
 		fade: true,
@@ -9,10 +9,19 @@ $(function () {
 		asNavFor: '.slider-dots' //связь
 	});
 
+	// работа полосочек внизу 1-го экрана
 	$('.slider-dots').slick({
 		slidesToShow: 4,
 		slidesToScroll: 4,
 		asNavFor: '.header__slider' //связь
+	});
+
+	// работа слайдов-картинок 2-го экрана и стрелочки (лево-право)
+	$('.surf-slider').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+		nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">'
 	});
 
 });
