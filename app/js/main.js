a6b5278cc99b2828eef6add586899978acd10aa9
sereja-s <serejasuvorov1979@gmail.com>
2021-06-27 14:36:54 +0300

    Второй экран 3-я часть (были слайды-картинки с кнопками, добавили слайд-табличку(без стилей)

--- app/js/main.js
+++ app/js/main.js
@@ -6,11 +6,11 @@ $(function () {
 		fade: true,
 		prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
 		nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
-		asNavFor: '.slider-dots' //связь
+		asNavFor: '.slider-dotshead' //связь
 	});
 
 	// работа полосочек внизу 1-го экрана
-	$('.slider-dots').slick({
+	$('.slider-dotshead').slick({
 		slidesToShow: 4,
 		slidesToScroll: 4,
 		asNavFor: '.header__slider' //связь
@@ -21,7 +21,18 @@ $(function () {
 		slidesToShow: 4,
 		slidesToScroll: 1,
 		prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
-		nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">'
+		nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">',
+		asNavFor: '.slider-map' //связь
 	});
+
+	// работа слайдов (точек и таблички) на карте
+	$('.slider-map').slick({
+		slidesToShow: 8,
+		slidesToScroll: 1,
+		arrows: false,
+		asNavFor: '.surf-slider', //связь
+		focusOnSelect: true // при щелчке в табличке находит нужный слайд(картинку)
+	});
+
 
 });
