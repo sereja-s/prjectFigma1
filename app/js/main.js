103ee138e794638e5fa6d105f24de116e820628d
sereja-s <serejasuvorov1979@gmail.com>
2021-06-28 20:20:33 +0300

    Третий экран

--- app/js/main.js
+++ app/js/main.js
@@ -9,11 +9,11 @@ $(function () {
 		asNavFor: '.slider-dotshead' //связь
 	});
 
-	// работа полосочек внизу 1-го экрана
+	// лниии(дотсы) снизу 1-го экрана окрашиваются в соответствии с наименованием на экране
 	$('.slider-dotshead').slick({
 		slidesToShow: 4,
 		slidesToScroll: 4,
-		asNavFor: '.header__slider' //связь
+		asNavFor: '.header__slider' //связь с работой стрелочек(вверху)
 	});
 
 	// работа слайдов-картинок 2-го экрана и стрелочки (лево-право)
@@ -32,6 +32,14 @@ $(function () {
 		arrows: false,
 		asNavFor: '.surf-slider', //связь
 		focusOnSelect: true // при щелчке в табличке находит нужный слайд(картинку)
+	});
+
+	// работа стрелочек (лево-право) на третьем экране
+	$('.travel__slider').slick({
+		infinite: true,
+		fade: true,
+		prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
+		nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">'
 	});
 
 
