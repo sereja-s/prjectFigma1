1cc4a9bbccceee0620bfa086e05d6b666b40ccc9
sereja-s <serejasuvorov1979@gmail.com>
2021-07-01 00:08:31 +0300

    4-й экран, 5-й зкран (1-я часть)

--- app/js/main.js
+++ app/js/main.js
@@ -35,12 +35,66 @@ $(function () {
 	});
 
 	// работа стрелочек (лево-право) на третьем экране
-	$('.travel__slider').slick({
+	$('.holder__slider').slick({
 		infinite: true,
 		fade: true,
 		prevArrow: '<img class="slider-arrows slider-arrows__left" src="images/arrows-left.svg" alt="">',
 		nextArrow: '<img class="slider-arrows slider-arrows__right" src="images/arrows-right.svg" alt="">'
 	});
+
+	//  Для кастомизации счётчиков на 4-ом экране (добавит + и -)
+	// ( код с сайта: https://codepen.io/komarovdesign/pen/PPRbgb )
+
+	// прописали картинки + и - в коде html
+	$('<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="images/plus.svg" alt=""></div><div class="quantity-button quantity-down"><img src="images/minus.svg" alt=""></div></div>').insertAfter('.quantity input');
+	$('.quantity').each(function () {
+		var spinner = $(this),
+			input = spinner.find('input[type="number"]'),
+			btnUp = spinner.find('.quantity-up'),
+			btnDown = spinner.find('.quantity-down'),
+			min = input.attr('min'),
+			max = input.attr('max');
+
+		btnUp.click(function () {
+			var oldValue = parseFloat(input.val());
+			if (oldValue >= max) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue + 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+		btnDown.click(function () {
+			var oldValue = parseFloat(input.val());
+			if (oldValue <= min) {
+				var newVal = oldValue;
+			} else {
+				var newVal = oldValue - 1;
+			}
+			spinner.find("input").val(newVal);
+			spinner.find("input").trigger("change");
+		});
+
+	});
+
+	// Формула расчёта суммы за проживание в гостинице (при нажатии на + или - калькулятора) 4-го экрана
+
+	$('.quantity-button').on('click', function () {
+
+		let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+
+		$('.summ').html('$' + summ);
+
+	});
+
+	let summ = $('.nights').val() * $('.summ').data('nights') + ($('.guests').val() - 1) * $('.summ').data('guests');
+
+	$('.summ').html('$' + summ);
+
+
+
 
 
 });
